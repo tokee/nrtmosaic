e60523373f32f400820ece2d4ede43c7004d995d
Toke Eskildsen <te@statsbiblioteket.dk>
2016-06-28 10:08:40 +0200

    Convenience deploy-script now also copies the gui to the tomcat

--- deployLocalTomcat.sh
+++ deployLocalTomcat.sh
@@ -3,7 +3,9 @@
 rm -r tomcat/webapps/nrtmosaic*
 mvn clean package -DskipTests
 mv target/nrtmosaic*.war target/nrtmosaic.war
-cp target/nrtmosaic.war tomcat/webapps/
 
-echo "nrtmosaic deployed to localtomcat"
+cp target/nrtmosaic.war tomcat/webapps/
+cp -r gui/ tomcat/webapps/
+
+echo "nrtmosaic deployed to local tomcat, available at http://localhost:8080/gui/"
 
